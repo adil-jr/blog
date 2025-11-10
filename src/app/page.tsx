2548b9a3919e3c1a1e1962d2e784d4e776523d19
adil-jr <adil.jr@outlook.com>
2025-11-10 04:07:00 -0300

    feat(ui): replace h1 with Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,9 @@
-import clsx from 'clsx';
+import { Header } from '@/components/Header';
 
 export default function HomePage() {
   return (
     <div>
-      <h1
-        className={clsx(
-          'text-6xl',
-          'font-bold',
-          'text-blue-500',
-          'hover:text-blue-50',
-          'hover:bg-blue-500',
-          'transition',
-          'duration-1000',
-        )}
-      >
-        Texto do meu h1
-      </h1>
+      <Header />
     </div>
   );
 }
